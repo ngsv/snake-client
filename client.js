acf01dcbe83b61fd5496e7730eb66bfc39db07af
Steven Ngov <gl.steven.ngov@gmail.com>
2021-12-05 09:59:30 +0000

    Initial commit

---
+++ client.js
@@ -0,0 +1,29 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here,
+    port: 50541 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: SN");
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+
+module.exports = {
+  connect
+};
