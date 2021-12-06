a8abe64306c544b0e30590b0436bb939786eac19
Steven Ngov <gl.steven.ngov@gmail.com>
2021-12-06 04:47:42 +0000

    Add input module

--- play.js
+++ play.js
@@ -1,24 +1,6 @@
-const {
-  connect
-} = require("./client");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 connect();
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
 setupInput();
