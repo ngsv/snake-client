b39ba3a60dd18d183e9161fe1ed7699b696cc444
Steven Ngov <gl.steven.ngov@gmail.com>
2021-12-06 05:26:52 +0000

    Add chat bubbles

--- input.js
+++ input.js
@@ -28,6 +28,12 @@ const handleUserInput = (key) => {
   if (key === 'd') {
     connection.write('Move: right');
   }
+  if (key === 'q') {
+    connection.write('Say: Hi there!');
+  }
+  if (key === 'e') {
+    connection.write("Say: I'm growing!");
+  }
 };
 
 handleUserInput();
