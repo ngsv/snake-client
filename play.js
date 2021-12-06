d525e9179acff3959c86d9d6cf5cbd30e30e8500
Steven Ngov <gl.steven.ngov@gmail.com>
2021-12-06 04:39:33 +0000

    Setup stdin

--- play.js
+++ play.js
@@ -1,3 +1,24 @@
-const { connect } = require("./client");
+const {
+  connect
+} = require("./client");
 
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
